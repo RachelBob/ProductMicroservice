2ecae07ed8ab1fe8744c1985293deeebcd8fc2e7
RachelBob <bobbyrachel@gmail.com>
2022-11-01 20:29:46 +0530

    Added additional products

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -9,6 +9,10 @@ insert into products(products_id, name, description, price, uuid, category, rati
 
 insert into products(products_id, name, description, price, uuid, category, rating, imagesrc) values (nextval('seq_product'),'vivo f19 Pro Plus','vivo f19 Pro Plus description',24000,md5(random()::text)::uuid, 'Mobile', 3.9, 'https://rukminim1.flixcart.com/image/416/416/kn97te80/mobile/p/m/9/f19-pro-5g-cph2213-oppo-original-imagfz38hbzjhkqx.jpeg?q=70');
 
+insert into products(products_id, name, description, price, uuid, category, rating, imagesrc) values (nextval('seq_product'),'SAMSUNG Galaxy Z Flip3 5G','SAMSUNG Galaxy Z Flip3 5G(Phantom Black, 128 GB)(8 GB RAM) description',84999,md5(random()::text)::uuid, 'Mobile', 4.4, 'https://rukminim1.flixcart.com/image/416/416/ksnjp8w0/mobile/f/k/y/galaxy-z-flip3-5g-sm-f711bzkainu-samsung-original-imag662axn2xmght.jpeg?q=70');
+
+insert into products(products_id, name, description, price, uuid, category, rating, imagesrc) values (nextval('seq_product'),'Xiaomi 12 Pro 5G','Xiaomi 12 Pro 5G (Opera Mauve, 256 GB)  (12 GB RAM) description',59990,md5(random()::text)::uuid, 'Mobile', 4.2, 'https://rukminim1.flixcart.com/image/416/416/kyoqmq80/mobile/f/m/b/11t-pro-5g-11t-mi-original-imagauz3cr3wzrhg.jpeg?q=70');
+
 insert into products(products_id, name, description, price, uuid, category, rating, imagesrc) values (nextval('seq_product'),'Apple 2020 Macbook Air M1','Apple 2020 Macbook Air M1 description',94990,md5(random()::text)::uuid, 'Laptop', 4.8, 'https://rukminim1.flixcart.com/image/416/416/khdqnbk0/computer/s/2/g/apple-original-imafxfyqykyghjg6.jpeg?q=70');
 
 insert into products(products_id, name, description, price, uuid, category, rating, imagesrc) values (nextval('seq_product'),'HP Core i5 12th Gen','HP Core i5 12th Gen description',64990,md5(random()::text)::uuid, 'Laptop', 4.5, 'https://rukminim1.flixcart.com/image/416/416/xif0q/computer/d/r/e/15s-fq5111tu-thin-and-light-laptop-hp-original-imagha3x4gbfwsdx.jpeg?q=70');
@@ -17,7 +21,11 @@ insert into products(products_id, name, description, price, uuid, category, rati
 
 insert into products(products_id, name, description, price, uuid, category, rating, imagesrc) values (nextval('seq_product'),'acer Aspire 7 Ryzen 5','acer Aspire 7 Ryzen 5 description',47990,md5(random()::text)::uuid, 'Laptop', 4.3, 'https://rukminim1.flixcart.com/image/416/416/xif0q/computer/b/e/h/-original-imagjyc69gh3a8wu.jpeg?q=70');
 
-insert into products(products_id, name, description, price, uuid, category, rating, imagesrc) values (nextval('seq_product'),'DELL G15 Core i7 11th Gen','DELL G15 Core i7 11th Gen description',99990,md5(random()::text)::uuid, 'Laptop', 4.2, 'https://rukminim1.flixcart.com/image/416/416/kqb8pzk0/computer/d/j/c/na-2-in-1-laptop-dell-original-imag4dy68bwtg8j9.jpeg?q=70');
+insert into products(products_id, name, description, price, uuid, category, rating, imagesrc) values (nextval('seq_product'),'DELL G15 Core i7 11th Gen','DELL G15 Core i7 11th Gen description',99990,md5(random()::text)::uuid, 'Laptop', 3.9, 'https://rukminim1.flixcart.com/image/416/416/kqb8pzk0/computer/d/j/c/na-2-in-1-laptop-dell-original-imag4dy68bwtg8j9.jpeg?q=70');
+
+insert into products(products_id, name, description, price, uuid, category, rating, imagesrc) values (nextval('seq_product'),'MSI Katana GF66','MSI Katana GF66 Core i7 11th Gen description',94990,md5(random()::text)::uuid, 'Laptop', 4.5, 'https://rukminim1.flixcart.com/image/416/416/kt1u3rk0/computer/w/u/g/katana-gf66-11ud-thin-and-light-laptop-msi-original-imag6hgufspmqjfr.jpeg?q=70');
+
+insert into products(products_id, name, description, price, uuid, category, rating, imagesrc) values (nextval('seq_product'),'acer Predator Helios 300','acer Predator Helios 300 Core i9 12th Gen description',99990,md5(random()::text)::uuid, 'Laptop', 4.7, 'https://rukminim1.flixcart.com/image/416/416/xif0q/computer/y/e/o/ph315-55-gaming-laptop-acer-original-imaghffqychub2fr.jpeg?q=70');
 
 insert into products(products_id, name, description, price, uuid, category, rating, imagesrc) values (nextval('seq_product'),'OnePlus U1S 164 cm (65 inch) Ultra HD (4K)','OnePlus U1S 164 cm description',75990,md5(random()::text)::uuid, 'Smart TV', 3.9, 'https://rukminim1.flixcart.com/image/416/416/kuwzssw0/television/i/q/x/u-series-50-u1s-50uc1a00-oneplus-original-imag7xtnfn7yrfrn.jpeg?q=70');
 
@@ -28,5 +36,11 @@ insert into products(products_id, name, description, price, uuid, category, rati
 insert into products(products_id, name, description, price, uuid, category, rating, imagesrc) values (nextval('seq_product'),'SAMSUNG 138 cm (55 inch) Ultra HD (4K)','SAMSUNG 138 cm (55 inch) Ultra HD (4K) description',92999,md5(random()::text)::uuid, 'Smart TV', 4.5, 'https://rukminim1.flixcart.com/image/416/416/xif0q/television/a/j/v/ua55bu8000klxl-samsung-original-imaggb4zpx37wgee.jpeg?q=70');
 
 insert into products(products_id, name, description, price, uuid, category, rating, imagesrc) values (nextval('seq_product'),'LG Nanocell 139 cm (55 inch) Ultra HD (4K)','LG Nanocell 139 cm (55 inch) Ultra HD (4K) description',89990,md5(random()::text)::uuid, 'Smart TV', 4.7, 'https://rukminim1.flixcart.com/image/416/416/jy3anbk0/television/z/7/y/lg-49sm8100pta-original-imafgcrpyhpdjgym.jpeg?q=70');
+
+insert into products(products_id, name, description, price, uuid, category, rating, imagesrc) values (nextval('seq_product'),'TCL C825 139 cm','TCL C825 139 cm (55 inch) QLED Ultra HD (4K) description',95990,md5(random()::text)::uuid, 'Smart TV', 4.9, 'https://rukminim1.flixcart.com/image/416/416/xif0q/television/c/7/y/-original-imaggaxbgbyky4vf.jpeg?q=70');
+
+insert into products(products_id, name, description, price, uuid, category, rating, imagesrc) values (nextval('seq_product'),'Vu GloLED 164 cm (65 inch)','Vu GloLED 164 cm (65 inch) description',61990,md5(random()::text)::uuid, 'Smart TV', 3.8, 'https://rukminim1.flixcart.com/image/416/416/xif0q/television/m/l/n/55gloled-vu-original-imaghzmnzdfzqkgt.jpeg?q=70');
+
+insert into products(products_id, name, description, price, uuid, category, rating, imagesrc) values (nextval('seq_product'),'TOSHIBA M550LP Series 164 cm (65 inch)','TOSHIBA M550LP Series 164 cm (65 inch) description',75990,md5(random()::text)::uuid, 'Smart TV', 4.5, 'https://rukminim1.flixcart.com/image/416/416/xif0q/television/y/k/r/-original-imagg9scfzezuwf4.jpeg?q=70');
 
 commit;

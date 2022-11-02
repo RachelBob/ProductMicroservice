57a0c2b6ec0830a69233e8ee12c0843df49d5c32
rohitkumargiri <girirohitkumar0@gmail.com>
2022-11-02 16:31:13 +0530

    Added ProductnotFound class

---
+++ src/main/java/com/lti/productservice/exception/ProductNotFoundException.java
@@ -0,0 +1,18 @@
+package com.lti.productservice.exception;
+
+
+public class ProductNotFoundException extends RuntimeException{
+
+private static final long serialVersionUID = 1L;
+	
+	public ProductNotFoundException() {
+		super();
+	}
+	
+	public ProductNotFoundException(String message) {
+		super(message);
+	}
+
+	
+	
+}
